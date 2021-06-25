55536fc2139d73421af3674fbe3dee5568e19f32
Fares Abousaif <faresabousaif@gmail.com>
2021-06-26 01:44:13 +0300

    Use the user class correctly.

--- user/Program.cs
+++ user/Program.cs
@@ -7,38 +7,17 @@ namespace user
     static void Main(string[] args)
     {
 
-      User test = new User("", "");
-      Console.WriteLine("enter username :  ");
-      string a = Console.ReadLine();
-      Console.WriteLine($"username is :  {test.username}");
+      // Get the username.
+      Console.Write("Username: ");
+      string username = Console.ReadLine();
 
-      Console.WriteLine("enter password :  ");
-      string b = Console.ReadLine();
-      Console.WriteLine($"password is :  {test.passwoord}");
+      // Get the password.
+      Console.Write("Password: ");
+      string password = Console.ReadLine();
 
-      test.Profail();
-      test.Start();
-
-      //Console.WriteLine(" enter uesr :");
-      //string u = Console.ReadLine();
-
-      //Console.WriteLine(" uesrname  is : ");
-      //Console.WriteLine(" password  is : ");
-
-      /*Print_useer a = new Print_useer();
-
-
-      Console.Write(a.username);
-      Console.Write(a.passwoord);*/
-
-      /* Console.ReadLine();
-       string f = a.username;
-       Console.WriteLine(f);
-      */
-      //Print_useer a = Console.ReadLine(Print_useer a.username)); ; ;
-      //Print_useer k = new Print_useer("n", "j", "h");
-
-      //Console.ReadLine();
+      // Create the new user instance.
+      User user = new User(username, password);
+      user.Profail();
     }
   }
 }
