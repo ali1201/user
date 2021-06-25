d2549b6b770480c3136098e20463ddf897b47088
Ali <85049617+ali1201@users.noreply.github.com>
2021-06-26 00:43:19 +0300

    Add project files.

---
+++ user/Print_useer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Print_useer
+{
+    public string username { set; get; }
+    public string passwoord { set; get; }
+    public string email;
+    public Print_useer(string u,string p)
+    {
+        username = u;
+        passwoord = p;
+    }
+    
+        public void Profail()
+        {
+            Console.WriteLine($"username is : {username}");
+            Console.WriteLine($"password is : {passwoord}********");
+            if (email == null)
+            {
+                Console.WriteLine(" email not found ");
+            }
+            else
+            {
+                Console.WriteLine($"emai is : {email}");
+            }
+        }
+        public void Start()
+        {
+            Console.WriteLine("starting....");
+        }
+    
+}
+   
+
---
+++ user/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace user
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Print_useer test = new Print_useer("","");
+            Console.WriteLine("enter username :  ");
+            string a = Console.ReadLine();
+            Console.WriteLine($"username is :  {test.username}");
+            
+            Console.WriteLine("enter password :  ");
+            string b = Console.ReadLine();
+            Console.WriteLine($"password is :  {test.passwoord}");
+
+           test.Profail();
+            test.Start();
+            
+            //Console.WriteLine(" enter uesr :");
+            //string u = Console.ReadLine();
+
+            //Console.WriteLine(" uesrname  is : ");
+            //Console.WriteLine(" password  is : ");
+
+            /*Print_useer a = new Print_useer();
+
+
+            Console.Write(a.username);
+            Console.Write(a.passwoord);*/
+
+            /* Console.ReadLine();
+             string f = a.username;
+             Console.WriteLine(f);
+            */
+            //Print_useer a = Console.ReadLine(Print_useer a.username)); ; ;
+            //Print_useer k = new Print_useer("n", "j", "h");
+
+            //Console.ReadLine();
+        }
+    }
+}
